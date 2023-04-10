d717f668fcc0003a4de3a9a3a09471f50590ef45
Hannah Fajardo <hanfajardo@gmail.com>
2023-04-10 17:29:57 +0800

    Update models relation, retrieve products from db

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
   def index
-    @products = GetProducts.call
+    @products = Product.all
     render json: @products
   end
 end
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,2 +1,4 @@
+# frozen_string_literal: true
 class Cart < ApplicationRecord
+  belongs_to :user
 end
